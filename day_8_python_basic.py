28a7c9c3973d1ab994afcf81a16d5e258de25d20
mahizhavarthiniseenivasan05 <mahizhavarthiniseenivasan05@gmail.com>
2026-06-26 18:59:35 +0530

    Create day_8_python_basic.py

---
+++ day_8_python_basic.py
@@ -0,0 +1,14 @@
+Day 8 - Lists, 
+Mutability 
+Backward Indexing Practice 
+
+
+fruits = ["Apple", "Banana", "Orange", "Mango"]
+
+print("Original List:", fruits)
+
+fruits[-2] = "Grapes"
+
+print("Updated List:", fruits)
+print("First Fruit:", fruits[0])
+print("Last Fruit:", fruits[-1])
