3bad6a6cb7ddfb783d674a9b7e96cc3fdaa2ca83
mahizhavarthiniseenivasan05 <mahizhavarthiniseenivasan05@gmail.com>
2026-07-01 21:55:18 +0530

    Create day10_tuple_basics.py

---
+++ day10_tuple_basics.py
@@ -0,0 +1,11 @@
+Day 10 - Python Tuple Basics and Single Element Tuple
+
+
+Tuple: (10, 20, 30, 40)
+Single Element Tuple: (100,)
+First Element: 10
+Last Element: 40
+Length: 4
+Minimum: 10
+Maximum: 40
+Type of single: <class 'tuple'>
