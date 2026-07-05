a3e8a978216a6eae743f1b0a25a8700b20f0aff8
mahizhavarthiniseenivasan05 <mahizhavarthiniseenivasan05@gmail.com>
2026-07-05 19:17:23 +0530

    Create day11_slicing_practice.py

---
+++ day11_slicing_practice.py
@@ -0,0 +1,22 @@
+Day 11 - String, List and Tuple Slicing Practice
+
+# Day 11 - Slicing Practice
+
+text = "PYTHON"
+print("String:", text)
+print("First 4 Letters:", text[:4])
+print("Reverse:", text[::-1])
+
+numbers = [10, 20, 30, 40, 50]
+print("List Slice:", numbers[1:4])
+
+marks = (80, 90, 100, 110, 120)
+print("Tuple Slice:", marks[2:])
+
+
+
+String: PYTHON
+First 4 Letters: PYTH
+Reverse: NOHTYP
+List Slice: [20, 30, 40]
+Tuple Slice: (100, 110, 120)
