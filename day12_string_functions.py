8607c0c7c4666207d452e4b505c2fc8ef0fb5747
mahizhavarthiniseenivasan05 <mahizhavarthiniseenivasan05@gmail.com>
2026-07-06 19:07:27 +0530

    Create day12_string_functions.py

---
+++ day12_string_functions.py
@@ -0,0 +1,28 @@
+Day 12 - Python String Functions Practice
+
+
+
+# Day 12 - String Functions
+
+text = "python programming"
+
+print("Original:", text)
+print("Upper:", text.upper())
+print("Lower:", text.lower())
+print("Title:", text.title())
+print("Capitalized:", text.capitalize())
+print("Length:", len(text))
+print("Count of 'm':", text.count("m"))
+print("Find 'programming':", text.find("programming"))
+print("Replace:", text.replace("python", "Java"))
+
+
+Original: python programming
+Upper: PYTHON PROGRAMMING
+Lower: python programming
+Title: Python Programming
+Capitalized: Python programming
+Length: 18
+Count of 'm': 2
+Find 'programming': 7
+Replace: Java programming
